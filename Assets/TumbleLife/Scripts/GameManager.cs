c708f52ad39525366d7cc97bab72a9f8c7d9e646
Quentin Doyonnas <qdoyonnas@gmail.com>
2019-02-28 13:09:17 -0500

    Initial Commit

---
+++ Assets/TumbleLife/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Camera mainCamera;
+
+    public ConfigurableJoint joint;
+
+
+    public TumbleBlock grabbedBlock = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joint = GetComponent<ConfigurableJoint>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Ray mouseRay = mainCamera.ScreenPointToRay(mousePos);
+        Vector3 mouseWorldPos = new Vector3(mouseRay.origin.x, mouseRay.origin.y, 0);
+        transform.position = mouseWorldPos;
+
+        if( Input.GetMouseButtonDown(0) && grabbedBlock == null ) {
+            RaycastHit hit;
+            if( Physics.Raycast(mouseRay, out hit) ) {
+                TumbleBlock blockScript = hit.collider.gameObject.GetComponent<TumbleBlock>();
+                if( blockScript != null && blockScript.isGrabbable ) {
+                    grabbedBlock = blockScript;
+                    joint.connectedBody = grabbedBlock.rigidBody;
+                }
+            }
+        }
+
+        if( Input.GetMouseButtonUp(0) && grabbedBlock != null ) {
+            joint.connectedBody = null;
+            grabbedBlock = null;
+        }
+    }
+}
---
+++ Assets/TumbleLife/Scripts/TumbleBlock.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TumbleBlock : MonoBehaviour
+{
+    public bool isGrabbable = true;
+    public Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+
+        Material mat = GetComponent<Renderer>().material;
+        Color color = Random.ColorHSV();
+        mat.color = color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
